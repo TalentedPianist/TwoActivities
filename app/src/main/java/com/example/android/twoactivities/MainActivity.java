7cde349ca4e097e9bdade5b42d6edd7eb5bdb57c
Douglas McGregor <41552151+TalentedPianist@users.noreply.github.com>
2024-02-09 11:03:14 +0000

    first commit

---
+++ app/src/main/java/com/example/android/twoactivities/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.twoactivities;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
+
+    }
+
+    private EditText mMessageEditText;
+
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    public void launchSecondActivity(View view) {
+
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/android/twoactivities/SecondActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // I had below code in MainActivity.java instead of SecondActivity.java and that's why it kept crashing.
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = (TextView) findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+}
